a9c031bd77321ac421a063e82ad6b1d6f72c00e0
agashs <srinivasanagash1978@gmail.com>
2025-03-18 11:30:10 +0530

    Add files via upload

---
+++ 2aserver.py
@@ -0,0 +1,8 @@
+#Name :Agash s
+#Reg no:212224040014
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+while True:
+ print(s.recv(1024).decode())
+ s.send("Acknowledgement Recived".encode())
